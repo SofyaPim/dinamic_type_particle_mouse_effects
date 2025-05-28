306ecddb47e54d5a4947d85eee93da0c58ca4608
Sofya <48826470+SofyaPim@users.noreply.github.com>
2025-05-28 14:04:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -66,7 +66,7 @@ window.addEventListener("DOMContentLoaded", function () {
       //     }
       //   })
         this.textInput.addEventListener("input", (e) => {
-        if (e.target.value.trim() !== "") {
+        if (e.target.value.trim() !== " ") {
           this.context.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
           this.wrapText(e.target.value);
         }
